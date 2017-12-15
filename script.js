d135208bef30f5ad64250c8cedee8635d9e81364
Hilyd <danjean.lydie@gmail.com>
2017-12-15 16:01:23 +0100

    changer le nom d'une image

---
+++ script.js
@@ -0,0 +1,13 @@
+var urlImg = document.querySelector("img").src;
+console.log(urlImg);
+console.log(urlImgLast);
+
+var urlImgLast = urlImg.substr(-1);
+var button = document.querySelector("button");
+
+
+    button.addEventListener('click', function(){
+        for (i=0 ; i < urlImg.length; i++) {
+            
+        }
+    })
