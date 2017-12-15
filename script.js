76a670dd09514caa14edfa223b6044d4df3dbe51
Hilyd <danjean.lydie@gmail.com>
2017-12-15 16:38:24 +0100

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+// Récupère l'Url de l'image
 var urlImg = document.querySelector("img").src;
 console.log(urlImg);
-console.log(urlImgLast);
+var Nb = 4;
 
-var urlImgLast = urlImg.substr(-1);
+
+
+// Bouton envoyer
 var button = document.querySelector("button");
 
-
+// Sur clic du bouton
     button.addEventListener('click', function(){
-        for (i=0 ; i < urlImg.length; i++) {
-            
-        }
-    })
+        
+    Nb = Nb-1;
+        
+        if (Nb === -1)
+            Nb = 4;
+        
+        // Récupère le début de l'url et concatène avec 4 et jpg
+        var urlImgLast = urlImg.substr(0,urlImg.indexOf(".jpg")-1) + Nb + ".jpg";
+        console.log(urlImgLast);
+        
+        //Remplace la valeur de l'url 
+        document.querySelector("img").src = urlImgLast;
+     })
